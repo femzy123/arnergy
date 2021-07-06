15746ef978ffbd94b4854c69e0b9d42ab907555e
femzy123 <femzy123@yahoo.com>
2021-07-06 20:13:32 +0100

    added product routes

---
+++ routes/products.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const { products, ROLE } = require("../data");
+const { authUser } = require("../permission/auth");
+
+router.get("/", (req, res) => {
+  res.status(200).json(products);
+});
+
+router.get("/:productId", setProduct, (req, res) => {
+  res.json(req.product);
+});
+
+router.post(
+  "/new",
+  authUser,
+  (req, res) => {
+    res.status(201).json(req.body);
+  }
+);
+
+router.delete("/productId", (req, res) => {
+  res.send("Product deleted");
+});
+
+function setProduct(req, res, next) {
+  const productId = parseInt(req.params.productId);
+  req.product = products.find((product) => product.id === productId);
+
+  if (req.product == null) {
+    res.status(404);
+    return res.send("Product not found");
+  }
+  next();
+}
+
+module.exports = router;
